b4b5edf596f4a0cb1faacff1aa05fd3f17a9df9a
Mugisha-isaac <mugishaisaac2001@gmail>
2022-05-21 22:36:18 +0200

    feat(AuthTokenFilter) Filtering jsonwebtoken

--- src/main/java/com/springauth/springauth/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/springauth/springauth/security/jwt/AuthTokenFilter.java
@@ -1,5 +1,11 @@
 package com.springauth.springauth.security.jwt;
 
+import com.springauth.springauth.security.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,11 +16,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
       try{
           String jwt = parseJwt(request);
-          if(jwt !=null && jwtUtils)
+          if(jwt !=null && jwtUtils.validateJwtToken(jwt)){
+              String username = jwtUtils.getUsernameFromToken(jwt);
+
+              UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+              UsernamePasswordAuthenticationToken authentication =
+                      new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+              authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+              SecurityContextHolder.getContext().setAuthentication(authentication);
+          }
       }
       catch(Exception e){
           logger.error("can not set user authentication: {}",e);
